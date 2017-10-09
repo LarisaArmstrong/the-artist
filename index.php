67b5c626a23f4b211f6134b6bc795b336e74097b
LarisaArmstrong <larisaarmstrong@Larisas-MacBook-Pro.local>
2017-10-09 10:39:01 +0200

    init

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<?php get_header() ?>
+  </head>
+  <body>
+    
+      
+   <header> 
+       
+   </header>
+      
+   <main>
+   </main>   
+      
+   <footer>
+   </footer>
+  
+  </body>
+</html>
+
